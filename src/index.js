83b53be5f65f9501e7d081c50870b85ba5241b15
skowolsk <31890900+skowolsk@users.noreply.github.com>
2018-03-04 09:52:50 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
-}
+    let zero = 0;
+    while(number){
+        number = (number/5)|0;
+        zero +=number;
+    }
+    return zero;
+};
